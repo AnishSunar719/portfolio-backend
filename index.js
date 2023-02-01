9e8eec5f03f28f697e6c6376601be5b0e168649c
AnishSunar719 <tha075bct009@tcioe.edu.np>
2023-02-01 10:24:49 +0545

    added cors config

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ import cors from "cors";
 
 dotenv.config()
 const app = express();
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin:"*",
+    origin: "*",
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.post("/contact", saveMessage);
 app.get("/message", readMessage);
@@ -19,10 +25,10 @@ app.get("/message", readMessage);
 mongoose.set("strictQuery", false);
 
 mongoose
-.connect(process.env.DB_URL)
-.then(
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running at ${process.env.PORT}`);
-    })
-)
-.catch((error) => console.log(`Error ${error}`));
+    .connect(process.env.DB_URL)
+    .then(
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running at ${process.env.PORT}`);
+        })
+    )
+    .catch((error) => console.log(`Error ${error}`));
